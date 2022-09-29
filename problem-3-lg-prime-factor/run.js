d4a17e301b9121bda3cace50e42bcc67e894cf11
Jeremy Kent <jkent@intuit.com>
2022-09-29 11:24:58 -0700

    prime ribs

--- problem-3-lg-prime-factor/run.js
+++ problem-3-lg-prime-factor/run.js
@@ -1,34 +1,86 @@
-const primes = (() => {
-    let largestChecked = 2
-    let 
-})();
+const makePrimer = () => {
+    let largestChecked = 2;
+    const knownPrimes = {
+        2: 2
+    };
+    const ithPrime = (i) => {
+        for (let k = 1; Object.keys(knownPrimes).length < i; k++) {
+            isPrime(k);
+        }
+        return Object.entries(knownPrimes)[i - 1][0];
+    };
+    function isPrime(n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n in knownPrimes) {
+            return true;
+        }
+        if (n <= largestChecked) {
+            return false;
+        }
+        for (let [k, largestMultiple] of Object.entries(knownPrimes)) {
+            k = Number.parseInt(k);
+            while (largestMultiple < n) {
+                largestMultiple += k;
+            }
+            knownPrimes[k] = largestMultiple;
+
+        }
+        for (let k = largestChecked + 1; k < n; k++) {
+            isPrime(k);
+        }
+        for (const i in knownPrimes) {
+            const p = knownPrimes[i];
+            if (n % p == 0) {
+                largestChecked = n;
+                return false;
+            }
+        }
+        largestChecked = n;
+        knownPrimes[n] = n;
+        return true;
+    };
+    const largestBelow = (max) => {
+
+        if (isPrime(max)) {
+            return max;
+        }
+        return knownPrimes[knownPrimes.length - 1];
+    };
+    const getKnownPrimes = () => knownPrimes;
+    const getLowestFactor = (n) => {
+        let i = 1;
+        let p;
+        do {
+            p = Number.parseInt(ithPrime(i));
+            if (!(n % p)) {
+                return p;
+            }
+            i++;
+        } while (p < Math.sqrt(n));
+        return n;
+    };
+    const getLargestFactor = (n) => {
+        const low = getLowestFactor(n);
+        if (low == n) {
+            return n;
+        }
+        return getLargestFactor(n / low);
+    };
+    return {
+        ithPrime,
+        isPrime,
+        largestBelow,
+        getKnownPrimes,
+        getLowestFactor,
+        getLargestFactor,
+    };
+};
 
 
 const tgt = process.argv[2];
 
+const primes = makePrimer();
 
-
-
-let sum = 0;
-
-const _fib = { 0: 0, 1: 1 };
-const fib = i => {
-    if (i in _fib) return _fib[i];
-    a = fib(i - 1);
-    b = fib(i - 2);
-    _fib[i] = a + b;
-    return _fib[i];
-};
-
-let i = 0;
-while (true) {
-    let f = fib(i);
-    if (f >= max) break;
-    if (!(f % 2)) {
-
-        sum += f;
-    }
-    i += 1;
-}
-
-console.log(sum);
+console.log(tgt, primes.getLargestFactor(tgt));
